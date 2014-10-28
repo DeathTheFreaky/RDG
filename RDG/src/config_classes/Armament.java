8d401e45ad28ab30c2ecfe0c64c0b70e3ea3686f
flohil <florian.hilbinger@hotmail.com>
2014-10-28 12:22:11 +0100

    Change "Class" to "Item Class" in Weapons, Armaments, Potions cause class is keyword in java

--- RDG/src/config_classes/Armament.java
+++ RDG/src/config_classes/Armament.java
@@ -1,5 +1,10 @@
 package config_classes;
 
 public class Armament {
+	
+	private String name, armor_class, type, image_big, image_small;
+	private float class_multiplier, stats_low_multiplier, stats_high_multiplier, armor, speed, bonus;
+	
+	
 
 }
