3a573f1cdde6060e9071eceaca2cfb19d3676771
flohil <florian.hilbinger@hotmail.com>
2014-12-08 22:56:57 +0100

    new alex stuff

--- RDG/src/fighting/Fight.java
+++ RDG/src/fighting/Fight.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
 import elements.Attack;
